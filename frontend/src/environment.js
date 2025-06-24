52e56fd35822d075f5db8fe0af1e9e0247154f7b
Hey-SauravGupta <heysauravgupta2004@gmail.com>
2025-06-24 21:19:39 +0530

    Fixed Babel plugin issue for Render deployment

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+    "https://apnavideocallbackend-6buu.onrender.com" :
 
     "http://localhost:8000"
 
